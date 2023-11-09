95511a80ee6aae9d4bc072a80c4cb8e6cf3567d0
tnromy <tuanromy@gmail.com>
2023-11-09 21:13:03 +0700

    add statefull naviation

--- lib/course_view.dart
+++ lib/course_view.dart
@@ -5,60 +5,128 @@ class CourseView extends StatefulWidget {
 
   final Map<String, dynamic> course;
 
-  
-
   @override
   State<CourseView> createState() => _CourseViewState();
 }
 
 class _CourseViewState extends State<CourseView> {
+  int courseState = 0;
+  Color nextColor = Colors.white;
+  Color preColor = Colors.grey;
+
+  @override
+  void setState(VoidCallback fn) {
+    // TODO: implement setState
+    super.setState(fn);
+
+    if(courseState > 0) {
+      preColor = Colors.white;
+    }
+    else {
+      preColor = Colors.grey;
+    }
+
+    if ((courseState + 1) <
+                          widget.course["course_details"].length) {
+                            nextColor = Colors.white;
+                          }
+                          else {
+                            nextColor = Colors.grey;
+                          }
+  }
+ 
   @override
   Widget build(BuildContext context) {
-    List<Widget> courseDetails = [];
-
-    if (widget.course["course_details"] != null &&
-    widget.course["course_details"] is List) {
-      int i = 1;
-      widget.course["course_details"].forEach((row) {
-        courseDetails.add(
-          Card(
-            shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-            elevation: 3,
-            color: Colors.white,
-            child: Container(
-            margin: EdgeInsets.all(10),
-            child: Column(children: [
-              Align(
-                alignment: Alignment.topLeft,
-                child: Text("Langkah " + i.toString() + ": " + row["title"], style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold))
-              ),
-              Align(
-                alignment: Alignment.topLeft,
-                child: Text(row["desc"]))
-            ],),
-          )
-          )
-        );
-        i++;
-      });
-      
-}
-    
     return Scaffold(
-      body: ListView(
+      body: Stack(
         children: [
-          Container(
-            margin: EdgeInsets.only(bottom: 10),
-            child: Center(child: Text(widget.course["course_name"], style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)))),
+          ListView(
+            children: [
+              Container(
+                  margin: EdgeInsets.only(bottom: 10),
+                  child: Center(
+                      child: Text(widget.course["course_name"],
+                          style: TextStyle(
+                              fontSize: 24, fontWeight: FontWeight.bold)))),
+              Container(
+                  margin: EdgeInsets.all(5),
+                  child: Text("Materi Pelatihan:",
+                      style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold))),
+              Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  elevation: 3,
+                  color: Colors.white,
+                  child: Container(
+                    margin: EdgeInsets.all(10),
+                    child: Column(
+                      children: [
+                        Align(
+                            alignment: Alignment.topLeft,
+                            child: Text(
+                                "Langkah " +
+                                    (courseState + 1).toString() +
+                                    ": " +
+                                    widget.course["course_details"][courseState]
+                                        ["title"],
+                                style: TextStyle(
+                                    fontSize: 16, fontWeight: FontWeight.bold))),
+                        Align(
+                            alignment: Alignment.topLeft,
+                            child: Text(widget.course["course_details"][courseState]
+                                ["desc"]))
+                      ],
+                    ),
+                  )),
+            ],
+          ),
 
-          Container(
-            margin: EdgeInsets.all(5),
-            child: Text("Materi Pelatihan:", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold))),
-
-          Column(
-            children: courseDetails,
+          Positioned.fill(
+            bottom: 20,
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: 150,
+                  child: ElevatedButton(
+                    child: Text("< Previous"),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: preColor,
+                    ),
+                    onPressed: () {
+                      if (courseState >= 1) {
+                        setState(() {
+                          courseState--;
+                        });
+                      }
+                      
+                    },
+                  ),
+                ),
+                SizedBox(
+                  width: 150,
+                  child: ElevatedButton(
+                    child: Text("Next >"),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: nextColor,
+                    ),
+                    onPressed: () {
+                      if ((courseState + 1) <
+                          widget.course["course_details"].length) {
+                        setState(() {
+                          courseState++;
+                        });
+                      }
+                      
+                    },
+                  ),
+                ),
+              ],
+                      ),
+            ),
           ),
         ],
       ),
